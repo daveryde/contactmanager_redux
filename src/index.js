9af1738cdc279783beb119cea09612ef9681775d
d.mcordova87@gmail.com <d.mcordova87@gmail.com>
2020-07-11 14:09:37 -0700

    move redux logic into index from app

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux 
+import { Provider } from 'react-redux';
+import store from './store';
+
+const rootElement = document.getElementById(('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, rootElement);
+
 registerServiceWorker();
